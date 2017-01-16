fb8fd55e00fcabc499199533b83868bbb6a076db
Rick MacConnell <rick@eightfoldpath.org>
2017-01-16 09:46:29 -0500

    Initial commit

---
+++ app/src/main/java/org/eightfoldpath/tourguide/DiningFragment.java
@@ -0,0 +1,23 @@
+package org.eightfoldpath.tourguide;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rick on 1/16/17.
+ */
+
+public class DiningFragment extends Fragment {
+       @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_page, container, false);
+
+        return rootView;
+    }
+
+}
